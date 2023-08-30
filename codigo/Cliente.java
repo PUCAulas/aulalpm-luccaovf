610f7c00518ebcd57ea5c7de07fb711e5821e5b5
lucasht02 <lucashemetrio@gmail.com>
2023-08-30 12:03:19 -0300

    Mudar construtor

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,16 +1,8 @@
 public class Cliente {
-    private Integer codigoCliente;
     public String nome;
-    
-    public Cliente(Integer codigoCliente, String nome) {
-        this.codigoCliente = codigoCliente;
+
+    public Cliente(String nome) {
         this.nome = nome;
-    }
-    public Integer getCodigoCliente() {
-        return codigoCliente;
-    }
-    public void setCodigoCliente(Integer codigoCliente) {
-        this.codigoCliente = codigoCliente;
     }
     public String getNome() {
         return nome;
