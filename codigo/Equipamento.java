58be26598d7cb6b605e7be0354e37eacea9780b0
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2023-09-03 19:25:57 -0300

    subindo o crud para equipamento

--- codigo/Equipamento.java
+++ codigo/Equipamento.java
@@ -52,42 +52,6 @@ public class Equipamento {
 
     }
 
-    // editEquipamento
-    void editEquipamento(int codigo) {
-        for (Equipamento equipamento : armazenamento) {
-            if (getCodigoEquipamento() == codigo) {
-                System.out.println("\nDigite o novo nome do equipamento: ");
-                tipoEquipamento = input.nextLine();
-                System.out.println("\nDigite a descricao do equipamento: ");
-                descricaoEquipamento = input.nextLine();
-                setTipoEquipamento(tipoEquipamento);
-                setDescricaoEquipamento(descricaoEquipamento);
-            } else {
-                System.out.println("Codigo: " + codigo + "Nao econtrado no sistema");
-            }
-        }
-
-    }
-
-    // remove equipamento
-    void removeEquipamento(int codigo) {
-        Equipamento equipamentoRemover = null;
-
-        for (Equipamento equipamento : armazenamento) {
-            if (equipamento.getCodigoEquipamento() == codigo) {
-                equipamentoRemover = equipamento;
-                break;
-            }
-        }
-
-        if (equipamentoRemover != null) {
-            armazenamento.remove(equipamentoRemover);
-            System.out.println("Equipamento removido com sucesso.");
-        } else {
-            System.out.println("Equipamento não encontrado no sistema.");
-        }
-    }
-
     void imprimir() {
         System.out.println("-------- Listagem Dos Equipamentos -------- ");
         System.out.println("\nNome Do Equipamento: " + tipoEquipamento);
