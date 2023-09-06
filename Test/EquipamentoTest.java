1add3398733be855385dae05f9d41261bc81a048
Ian Nascimento Rocha <101294641+iannr@users.noreply.github.com>
2023-09-06 11:45:51 -0300

    Adicionando Test void addEquipamento()

--- Test/EquipamentoTest.java
+++ Test/EquipamentoTest.java
@@ -1,16 +1,29 @@
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class EquipamentoTest {
 
-	Equipamento equipamento = new Equipamento("Britadeira", "Quebrar pedras", 2);
+	Equipamento equipamento = new Equipamento("Estante", "guardar remedios", 2);
+	
 	
 	@Test
 	void verificarSeEquipamentoExiste() {
 		assertEquals(2, equipamento.getCodigoEquipamento());
 	}
 	
+	@Test
+	void addEqp() {
+		Equipamento equipamento = new Equipamento("Estante", "guardar remedios", 2);
+        equipamento.armazenamento.add(equipamento);
+		assertEquals("Estante",equipamento.getTipoEquipamento());
+		assertEquals("guardar remedios",equipamento.getDescricaoEquipamento());
+		assertEquals(2,equipamento.getCodigoEquipamento());
+
+
+
+	}
 	
 
 }
